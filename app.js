56bd73b8784ace56843bdaeb33bb81ad073d05a1
Derlis Dominguez <drewdominguez.dd@gmail.com>
2017-04-17 18:52:42 -0400

    Introduccion a JS

---
+++ app.js
@@ -0,0 +1,36 @@
+var i= 10;
+var nombre = 'Derlis';
+
+var f = function (argument) {
+  alert('saludar');
+}
+
+
+var app={
+  contar : function () {
+    for (var i =0; i < 100; i++) {
+      alert('Contador '+i);
+    };
+  },
+  par: function (num) {
+    if (num%2==0) {
+        return true;
+    }else{
+      return false;
+    }
+  },
+  ciclo : function(){
+    for(var i=1; i<=10; i++ ){
+      if (this.par(i)) {
+        alert(i+ 'es par');
+      }
+      else{
+        alert(i+ 'es ipar');
+      }
+    }
+  }
+};
+
+app.ciclo();
+
+  
